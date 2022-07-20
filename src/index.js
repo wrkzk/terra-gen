f6c62181b5a2f906f3c3a71602b04ffa3b32aa5d
wrkzk <wrkzk@protonmail.com>
2022-07-20 18:13:35 -0500

    Update terrain generation methods

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import SimplexNoise from 'simplex-noise';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
-var scene, camera, canvas, renderer, controls, simplex, gui;
-var material, mesh;
+var scene, camera, canvas, renderer, controls, gui, mesh;
+var generators;
 
 var terrain_properties = new function() {
     this.frequency = 0.04;
-    this.width = 50;
-    this.height = 50;
+    this.amplitude = 2;
+
+    this.width = 150;
+    this.height = 150;
 }
 
 function map(val, min1, max1, min2, max2) {
@@ -18,41 +20,43 @@ function map(val, min1, max1, min2, max2) {
     return (max2 - min2) * t + min2;
 }
 
-function noise(x, y) {
-    return map(simplex.noise2D(x, y), -1, 1, 0, 1);
+function noise(x, y, iter) {
+    return generators[iter].noise2D(x, y);
 }
 
-function octave(x, y, octaves) {
+function compound_noise(x, y, octaves) {
     let val = 0;
-    let freq = terrain_properties.frequency;
-    let max = 0;
+    let freq = 0.01;
     let amp = 1;
+    let sum = 0;
+
     for (let i=0; i < octaves; i++) {
-        val += noise(x * freq, y * freq) * amp;
-        max += amp;
+        val += noise(x * freq, y * freq, i) * amp;
+        sum += amp;
+
         amp /= 2;
         freq *= 2;
     }
-    return val/max;
+    return val/sum;
 }
 
-function render_terrain() {
-    var geometry = new THREE.PlaneGeometry(terrain_properties.width, terrain_properties.height, terrain_properties.width - 1, terrain_properties.height - 1);
-    apply_noise(geometry);
-    
-    material = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true});
+function init_terrain() {
+    let geometry = new THREE.PlaneGeometry(terrain_properties.width, terrain_properties.height, terrain_properties.width - 1, terrain_properties.height - 1);
+    let material = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true});
+
     mesh = new THREE.Mesh(geometry, material);
     mesh.rotation.x = Math.PI / 2
     
+    apply_noise(geometry);
     scene.add(mesh);
 }
 
-function update_terrain() {
-    var newGeo = new THREE.PlaneGeometry(terrain_properties.width, terrain_properties.height, terrain_properties.width - 1, terrain_properties.height - 1);
-    apply_noise(newGeo);
+function update_geometry() {
+    var geometry = new THREE.PlaneGeometry(terrain_properties.width, terrain_properties.height, terrain_properties.width - 1, terrain_properties.height - 1);
+    apply_noise(geometry);
 
     mesh.geometry.dispose();
-    mesh.geometry = newGeo;
+    mesh.geometry = geometry;
 }
 
 function apply_noise(geo) {
@@ -60,12 +64,13 @@ function apply_noise(geo) {
 
     for (let i = 0; i < terrain_properties.width; i++) {
         for (let j = 0; j < terrain_properties.height; j++) {
-            let gen = map(octave(j, i, 1), 0, 1, -10, 10);
+            //let gen = map(octave(j, i, 1), 0, 1, -10, 10) * terrain_properties.amplitude;
+            let gen = map(compound_noise(j, i, 5), -1, 1, -10, 10) * 3;
             let elevation;
-
+ 
             if (gen < 0) elevation = gen
             else elevation = 0
-
+            
             positions[(i * terrain_properties.width + j) * 3 + 2] = elevation;
         }
     }
@@ -77,11 +82,13 @@ function init() {
     canvas = document.querySelector("#c");
     renderer = new THREE.WebGLRenderer({canvas, antialias: true});
     controls = new OrbitControls(camera, renderer.domElement);
-    simplex = new SimplexNoise();
-    gui = new dat.GUI();
+    //gui = new dat.GUI();
+
+    generators = [new SimplexNoise(), new SimplexNoise(), new SimplexNoise(), new SimplexNoise(), new SimplexNoise()];
 
     // Create the gui
-    gui.add(terrain_properties, 'frequency', 0, 1).onChange(update_terrain);
+    //gui.add(terrain_properties, 'frequency', 0, 0.2).onChange(update_geometry);
+    //gui.add(terrain_properties, 'amplitude', 0, 2).onChange(update_geometry);
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0xf3f2e5);
@@ -91,7 +98,7 @@ function init() {
 
     document.body.appendChild(renderer.domElement);
 
-    camera.position.set(0, 45, 60);
+    camera.position.set(0, 120, 150);
     camera.lookAt(0, 0, 0);
     controls.update();
 }
@@ -102,5 +109,5 @@ function animate() {
 };
 
 init();
-render_terrain();
+init_terrain();
 animate();
